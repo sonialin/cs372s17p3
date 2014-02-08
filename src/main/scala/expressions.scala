b56fde21f81aae1dd3fd6fa9f803622b90e08faa
Konstantin Laufer <laufer@cs.luc.edu>
2014-02-08 15:01:20 -0600

    minor cosmetic change

--- src/main/scala/expressions.scala
+++ src/main/scala/expressions.scala
@@ -6,8 +6,8 @@ package edu.luc.cs.laufer.cs473.expressions
 sealed trait Expr
 case class Constant(value: Int) extends Expr
 case class UMinus(expr: Expr) extends Expr
-case class Plus(val left: Expr, val right: Expr) extends Expr
-case class Minus(val left: Expr, val right: Expr) extends Expr
-case class Times(val left: Expr, val right: Expr) extends Expr
-case class Div(val left: Expr, val right: Expr) extends Expr
-case class Mod(val left: Expr, val right: Expr) extends Expr
+case class Plus(left: Expr, right: Expr) extends Expr
+case class Minus(left: Expr, right: Expr) extends Expr
+case class Times(left: Expr, right: Expr) extends Expr
+case class Div(left: Expr, right: Expr) extends Expr
+case class Mod(left: Expr, right: Expr) extends Expr
