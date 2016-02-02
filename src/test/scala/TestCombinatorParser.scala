23d7eef7ec7eb4c8453f2c8be7fbfc6259b3442b
Konstantin Läufer <laufer@cs.luc.edu>
2016-02-02 05:14:22 +0000

    brought back tests for combinator parsers
    Signed-off-by: Konstantin Läufer <laufer@cs.luc.edu>

--- src/test/scala/TestCombinatorParser.scala
+++ src/test/scala/TestCombinatorParser.scala
@@ -4,17 +4,17 @@ import org.scalatest.FunSuite
 
 import TestFixtures._
 
-object MainPegParser extends App {
-  val parsedExpr = new PegParser(complex1string).InputLine.run()
+object MainCombinatorParser extends App {
+  val parsedExpr = CombinatorParser.parseAll(CombinatorParser.expr, complex1string)
   println(parsedExpr.get)
   println(complex1)
   println(parsedExpr.get == complex1)
   println(behaviors.evaluate(parsedExpr.get))
 }
 
-class TestPegParser extends FunSuite {
-  val parsedExpr = new PegParser(complex1string).InputLine.run()
-  val parsedExpr2 = new PegParser(complex1string2).InputLine.run()
+class TestCombinatorParser extends FunSuite {
+  val parsedExpr = CombinatorParser.parseAll(CombinatorParser.expr, complex1string)
+  val parsedExpr2 = CombinatorParser.parseAll(CombinatorParser.expr, complex1string2)
   test("parser works 1") { assert(parsedExpr.get === complex1) }
   test("parser works 2") { assert(parsedExpr2.get === complex1) }
 }
